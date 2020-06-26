83736713336150a0fa49f79f0a28d7593cc4bb73
ariiyantto168 <ariiyantto168@gmail.com>
2020-06-26 16:39:11 +0700

    updated front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,7 @@
 import React from 'react';
-import {
-   BrowserRouter as Router, 
-   Route,
-   Redirect,
-   Switch 
-  } from 'react-router-dom';
-
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 const App = () => {
-  return (
-    <Router>
-      <MainNavigation />
-      <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/:userId/places" exact>
-          <UserPlaces />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-      </main>
-    </Router>
-  );
-};
+    return <p>tes</p>
+}
 
 export default App;
-
-//point 49
